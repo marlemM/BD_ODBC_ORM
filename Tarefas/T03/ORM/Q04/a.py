e42aa77de06e9f4d562ab3c2e18efc0f9aa18633
20200120712 <danielmedeiros200@gmail.com>
2023-06-01 17:05:02 +0000

    #2 Q04//L(a)

--- Tarefas/T03/ORM/Q04/a.py
+++ Tarefas/T03/ORM/Q04/a.py
@@ -2,67 +2,67 @@ from faker import Faker
 import random
 import psycopg2
 
-# Função para gerar dados falsos
-def generate_fake_data(fake, num_rows):
-    # Gere dados falsos para a tabela "funcionarios"
-    funcionarios = []
-    for _ in range(num_rows):
-        descricao = fake.sentence(nb_words=6)
-        if len(descricao) > 45:
-            descricao = descricao[:45]  # Ajustar para 45 caracteres, se exceder
-        funcionarios.append(descricao)
+# Conectando ao banco de dados
+conn = psycopg2.connect(database="equipebd", user="postgres", password="admin", host="localhost")
+cur = conn.cursor()
 
-    # Gere dados falsos para a tabela "projeto"
-    projetos = []
-    for _ in range(num_rows):
-        nome = fake.company()
-        projetos.append(nome)
+# Instanciando o objeto Faker
+fake = Faker()
 
-    # Retorne os dados gerados
-    return funcionarios, projetos
+# Função para gerar um número aleatório de funcionários
+# Função para gerar um número aleatório de funcionários
+def gerar_funcionarios(num_funcionarios):
+    for _ in range(num_funcionarios):
+        nome = fake.name()[:15]  # Limita o tamanho do nome para 15 caracteres
+        # Resto do código
+        sexo = random.choice(['M', 'F'])
+        data_nasc = fake.date_of_birth(minimum_age=18, maximum_age=65)
+        salario = random.uniform(1000, 10000)
+        supervisor = random.choice(range(1, num_funcionarios+1))
+        depto = random.choice(range(1, 3))
+        query = "INSERT INTO funcionario (nome, sexo, dataNasc, salario, supervisor, depto) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (nome, sexo, data_nasc, salario, supervisor, depto)
+        cur.execute(query, values)
 
+# Função para gerar um número aleatório de departamentos
+def gerar_departamentos(num_departamentos):
+    for _ in range(num_departamentos):
+        sigla = fake.random_uppercase_letter() + fake.random_uppercase_letter() + fake.random_uppercase_letter()
+        descricao = fake.word()
+        gerente = random.choice(range(1, num_departamentos+1))
+        query = "INSERT INTO departamento (sigla, descricao, gerente) VALUES (%s, %s, %s)"
+        values = (sigla, descricao, gerente)
+        cur.execute(query, values)
 
+# Função para gerar um número aleatório de equipes
+def gerar_equipes(num_equipes):
+    for _ in range(num_equipes):
+        nome_equipe = fake.company_suffix()
+        query = "INSERT INTO equipe (nomeEquipe) VALUES (%s)"
+        values = (nome_equipe,)
+        cur.execute(query, values)
 
-# Função para popular as tabelas do banco de dados com dados falsos
-def populate_database(num_rows):
-    # Conectar ao banco de dados PostgreSQL
-    conn = psycopg2.connect(
-        host='localhost',
-        database='equipebd',
-        user='postgres',
-        password='admin'
-    )
+# Função para gerar um número aleatório de membros
+def gerar_membros(num_membros, num_funcionarios, num_equipes):
+    for _ in range(num_membros):
+        cod_equipe = random.choice(range(1, num_equipes+1))
+        cod_funcionario = random.choice(range(1, num_funcionarios+1))
+        query = "INSERT INTO membro (codEquipe, codFuncionario) VALUES (%s, %s)"
+        values = (cod_equipe, cod_funcionario)
+        cur.execute(query, values)
 
-    # Criar um cursor
-    cur = conn.cursor()
+# Definindo o número de linhas para cada tabela
+num_funcionarios = 10
+num_departamentos = 2
+num_equipes = 2
+num_membros = 8
 
-    try:
-        fake = Faker()
-        funcionarios, projetos = generate_fake_data(fake, num_rows)
+# Populando as tabelas
+gerar_funcionarios(num_funcionarios)
+gerar_departamentos(num_departamentos)
+gerar_equipes(num_equipes)
+gerar_membros(num_membros, num_funcionarios, num_equipes)
 
-        # Insira os dados na tabela "funcionarios"
-        for descricao in funcionarios:
-            cur.execute('INSERT INTO funcionarios (descricao) VALUES (%s)', (descricao,))
-        print(f'{len(funcionarios)} linhas inseridas na tabela "funcionarios"')
-
-        # Insira os dados na tabela "projeto"
-        for nome in projetos:
-            cur.execute('INSERT INTO projeto (nome) VALUES (%s)', (nome,))
-        print(f'{len(projetos)} linhas inseridas na tabela "projeto"')
-
-        # Efetive as alterações no banco de dados
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        conn.rollback()
-    finally:
-        # Fechar a comunicação com o PostgreSQL
-        cur.close()
-        conn.close()
-        print('Conexão com o banco de dados fechada.')
-
-# Número de linhas a serem geradas
-num_rows = 10
-
-# Popule as tabelas do banco de dados com dados falsos
-populate_database(num_rows)
+# Commit das alterações e fechamento da conexão
+conn.commit()
+conn.close()
