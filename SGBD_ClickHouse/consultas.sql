2d070a8a95735d4e6a90463869ee79cc204b24b0
20200120712 <112270865+20200120712@users.noreply.github.com>
2023-07-13 14:42:55 +0000

    consultas no sgbd

---
+++ SGBD_ClickHouse/consultas.sql
@@ -0,0 +1,23 @@
+///Consulta de todos os clientes:
+SELECT * FROM clientes;
+
+///Consulta de todos os itens do cardápio:
+SELECT * FROM cardapio;
+
+///Consulta de todos os itens do estoque:
+SELECT * FROM estoque;
+
+///Consulta de todos os pedidos:
+SELECT * FROM pedidos;
+
+///Consulta de todos os funcionários:
+SELECT * FROM funcionarios;
+
+///Consulta de todas as transações financeiras:
+SELECT * FROM transacoes;
+
+///Consulta de todos os fornecedores:
+SELECT * FROM fornecedores;
+
+////////////////////////////////////////////////////////////////
+
